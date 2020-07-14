1597b101b5e312084c55a16ef687502a5355af86
JackMcCabe548 <jackmccabe628@yahoo.co.uk>
2020-07-14 21:09:19 +0100

    version 0.13

--- src/FirstClass.java
+++ src/FirstClass.java
@@ -1,5 +1,17 @@
 public class FirstClass {
+
+
     public static void main (String [] args) {
-        System.out.println("Hello");
+
+        System.out.println(anus(5)); // For calling method with argument
+    }
+
+    public static int anus (int n) { // Method for calculation factorials
+        if (n <= 1) { // Factorial of 1 is 1
+            return 1;
+        }
+        else
+            return n*anus(n-1); // Method class itself (self referential)
+                                // Method will stop calling itself when (n-1) =0
     }
 }
